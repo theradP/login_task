c41419384db4422afae5075090f9aa45a7403cf1
theradP <mpranjal380@gmail.com>
2020-04-08 23:55:29 +0530

    added logout to dashboard

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Userip(models.Model):
+    user_ip = models.TextField(null=True)
+    date  = models.DateTimeField(default=datetime.datetime.now)
+    class Meta:
+        db_table = 'userip'
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-# from django.contrib.auth.forms import UserCreationForm
 from .forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from ipware import get_client_ip
+from .models import *
 # Create your views here.
 def indexView(request):
     return render(request, 'index.html')
@@ -11,6 +12,10 @@ def dashboardView(request):
     return render(request, 'dashboard.html')
 
 def registerView(request):
+    ip = get_client_ip(request)
+    user = Userip(user_ip=ip[0])
+    user.save()
+    # print('---------------',type(ip[0]),ip[0])
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
